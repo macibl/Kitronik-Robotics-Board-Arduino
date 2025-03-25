6ea693456434ce5daeef59db17bb7741a082f779
macibl <marc.hiebel@laposte.net>
2025-03-25 21:39:09 +0100

    Create KitronikNano33ble.ino
    1er commit .ino principal

---
+++ KitronikNano33ble.ino
@@ -0,0 +1,6 @@
+# Use a Kitronik Robotics board with a Arduino Nano 33 BLE using I2C
+# Restrictions: only motors can be controlled
+# 
+# based on https://github.com/KitronikLtd/Kitronik-Pico-Robotics-Board-MicroPython/blob/main/PicoRobotics.py
+#
+#include <Arduino.h>
